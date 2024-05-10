ec8b01b90324d27bc782030aeb4c76063451330d
davodhiambo <jesseomolo@yahoo.com>
2024-05-10 12:37:18 +0300

    doc: fix comments

--- mirrorFunction.go
+++ mirrorFunction.go
@@ -15,7 +15,7 @@ func vowelMirror(s string) string {
 		}
 	}
 
-	//switch vowel to mirror
+	//switch vowels to mirror
 	for i := 0; i < len(rnSlc); i++ {
 		for j := 0; j < len(contVowels); j++ {
 			if contVowels[j] == rnSlc[i] {
